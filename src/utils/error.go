191645c8165f1661cf5a744ae2c0612ef428f095
Rahuletto <rahulmarban@gmail.com>
2025-01-18 22:00:45 +0530

    fix: camelcase

--- src/utils/error.go
+++ src/utils/error.go
@@ -12,9 +12,9 @@ func HandleError(c *fiber.Ctx, err error) error {
 	if err != nil && (strings.Contains(err.Error(), "invalid response format") ||
 		strings.Contains(err.Error(), "invalid token format")) {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"token_invalid": true,
-			"error":         "Session expired or invalid",
-			"status":        fiber.StatusUnauthorized,
+			"tokenInvalid": true,
+			"error":        "Session expired or invalid",
+			"status":       fiber.StatusUnauthorized,
 		})
 	}
 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
